452e4401cf3a6ddb1ff93f9acc83f26eed16c1af
Timothy Luk <212979d@mymail.nyp.edu.sg>
2023-10-11 11:26:41 +0800

    Fixed frenzy UI + Added audio classes

---
+++ Assets/Scripts/Audio/SoundUID.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Audio
+{
+    // The unique idenfiers of a audio track
+    public enum SoundUID
+    {
+        NONE,
+        TEST_AUDIO,
+    }
+}
